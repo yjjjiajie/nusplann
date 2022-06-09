f202958afea1cf41166fffd8ec2d1d25c0168aa0
jj <yjjjiajie@gmail.com>
2022-06-09 16:57:20 +0800

    Configuration to deploy to heroku

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../components/moduleinfo/ModuleInfo.vue')
+  },
+  {
+    path: '/acadplan',
+    name: 'acadplan',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../components/acadplan/AcadPlan.vue')
   }
 ]
 
